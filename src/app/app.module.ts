cde892d9db21cec68f480d65039ed3aa47d194ff
Helder <h.t.pires84@gmail.com>
2021-10-13 14:55:55 +0200

    import  MatListModule, started countries.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatListModule} from '@angular/material/list';
+
 import { WeatherComponent } from './weather/weather.component';
 import { CountriesComponent } from './countries/countries.component';
+import { APIService } from './api.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,10 @@ import { CountriesComponent } from './countries/countries.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [APIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
