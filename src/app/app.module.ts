42d3fc376620bf4382e8ce9c6db4b7622ebddc5f
Helder <h.t.pires84@gmail.com>
2021-10-13 14:44:52 +0200

    added CountriesComponent, WeatherComponent and APIService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherComponent } from './weather/weather.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
