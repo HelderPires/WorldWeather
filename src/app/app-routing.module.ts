612e9ebc1cabd734018c813eecbd331841092f63
Helder <h.t.pires84@gmail.com>
2021-10-13 14:48:36 +0200

    added <router-outlet> to app.component and routing table to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
+import { CountriesComponent } from './countries/countries.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'weather/:country',
+    component: WeatherComponent,
+  },
+  {
+    path: 'countries',
+    component: CountriesComponent
+  },
+
+  {
+    path: '',
+    component: CountriesComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
